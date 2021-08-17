288d52bc0a88c3b197c4ddae48e62c9dc50a03c2
phucthinh12121999 <phucthinh12121999@gmail.com>
2021-08-17 12:47:57 +0700

    edit navbar and add link to navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 jQuery(document).ready(function ($) {
+  $(".smothscroll").on("click", function (e) {
+    e.preventDefault();
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 62,
+          },
+          1500,
+          "easeInOutExpo"
+        );
+      }
+    }
+  });
   $(window).load(function () {
     "use strict";
     // makes sure the whole site is loaded
